fd85e94ee100139d15b45bbc0fca8f52105c448f
bokul <bokul@horoppa.com>
2013-12-08 12:31:31 +0600

    db

--- SQL/account_manager.sql
+++ SQL/account_manager.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 07, 2013 at 01:30 PM
+-- Generation Time: Dec 08, 2013 at 12:31 PM
 -- Server version: 5.1.63-0ubuntu0.11.04.1
 -- PHP Version: 5.3.5-1ubuntu7.11
 
@@ -24,7 +24,7 @@ DROP TABLE IF EXISTS `bills`;
 CREATE TABLE IF NOT EXISTS `bills` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `particulars` varchar(255) NOT NULL,
-  `amount` float NOT NULL,
+  `amount` varchar(50) NOT NULL,
   `voucher_no` varchar(255) NOT NULL,
   `project_id` int(11) NOT NULL,
   `create_date` date NOT NULL,
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `bills` (
 --
 
 INSERT INTO `bills` (`id`, `particulars`, `amount`, `voucher_no`, `project_id`, `create_date`, `modify_date`) VALUES
-(1, '1st RA Bill', 55400, '1', 4, '2013-12-07', '2013-12-07 07:08:53');
+(1, '1st RA Bill', '55400', '1', 4, '2013-12-07', '2013-12-07 07:08:53');
 
 -- --------------------------------------------------------
 
@@ -49,20 +49,23 @@ DROP TABLE IF EXISTS `expenses`;
 CREATE TABLE IF NOT EXISTS `expenses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `particulars` varchar(255) NOT NULL,
-  `amount` float NOT NULL,
+  `amount` varchar(50) NOT NULL,
   `voucher_no` varchar(255) NOT NULL,
   `project_id` int(11) NOT NULL,
   `create_date` date NOT NULL,
   `modify_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `expenses`
 --
 
 INSERT INTO `expenses` (`id`, `particulars`, `amount`, `voucher_no`, `project_id`, `create_date`, `modify_date`) VALUES
-(1, 'Office', 100000, '1', 4, '2013-12-07', '2013-12-07 07:19:19');
+(1, 'Office', '100000', '1', 4, '2013-12-07', '2013-12-07 07:19:19'),
+(3, 'Photo Copy', '10000', '2', 4, '2013-12-07', '2013-12-07 15:37:52'),
+(4, 'Photo Copy', '5000', '3', 4, '2013-12-08', '2013-12-08 05:56:55'),
+(5, 'Office', '10000', '4', 4, '2013-12-08', '2013-12-08 05:57:44');
 
 -- --------------------------------------------------------
 
@@ -75,22 +78,23 @@ CREATE TABLE IF NOT EXISTS `projects` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `project_name` varchar(255) NOT NULL,
   `work_name` varchar(255) NOT NULL,
-  `work_order_amount` int(11) NOT NULL,
+  `work_order_amount` varchar(50) NOT NULL,
+  `address` varchar(255) NOT NULL,
   `create_date` date NOT NULL,
   `modify_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `projects`
 --
 
-INSERT INTO `projects` (`id`, `project_name`, `work_name`, `work_order_amount`, `create_date`, `modify_date`) VALUES
-(1, 'Kendua', 'Tubwell', 100000, '2013-10-11', '2013-11-29 15:32:08'),
-(2, 'Kendua', 'Tubwell', 100000, '2013-10-11', '2013-11-29 15:32:37'),
-(3, 'Kendua2', 'Tubwell', 100000, '2013-10-11', '2013-11-29 15:33:42'),
-(4, 'Kendua4', 'asdasd', 0, '2013-10-11', '2013-12-06 14:23:18'),
-(6, '', '', 0, '0000-00-00', '2013-12-07 07:24:58');
+INSERT INTO `projects` (`id`, `project_name`, `work_name`, `work_order_amount`, `address`, `create_date`, `modify_date`) VALUES
+(1, 'Kendua', 'Tubwell', '100000', '', '2013-10-11', '2013-11-29 15:32:08'),
+(2, 'Kendua', 'Tubwell', '100000', '', '2013-10-11', '2013-11-29 15:32:37'),
+(3, 'Kendua2', 'Tubwell', '100000', '', '2013-10-11', '2013-11-29 15:33:42'),
+(4, 'Kendua4', 'Tubwell', '1000000', 'Dhaka', '2013-10-11', '2013-12-08 05:38:56'),
+(7, 'Kendua5', 'Tubwell', '100000', 'asd', '2013-12-07', '2013-12-07 16:06:59');
 
 -- --------------------------------------------------------
 
